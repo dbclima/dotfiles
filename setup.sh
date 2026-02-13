e6dbfdec81a04d3a204584ad10252777896c058f
Diogo Lima <dev.diogolima@gmail.com>
2026-02-13 00:32:28 -0300

    Adição de script para criação de links simbolicos automaticos

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Setup de links simbólicos para a configuração de ambiente
+# leia [[ ./README.md ]] para concluir a instalação
+
+# link simbólico para os arquivos bash
+ln -s ./bash ~/.config/bash
+
+# link simbólico para os arquivos nvim
+ln -s ./nvim ~/.config/nvim
+
+# link simbólico para os arquivos tmux
+ln -s ./tmux ~/.config/tmux
+
