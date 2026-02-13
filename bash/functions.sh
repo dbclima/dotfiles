6203855fd2fe79d685503054791720487245f523
Diogo Lima <dev.diogolima@gmail.com>
2026-02-12 23:20:14 -0300

    Abre automaticamente o neovim no arquivo TODO caso exista

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,6 +1,15 @@
 # Criar funções customizadas
 
-# Exemplo: Funcao que alem de criar o diretorio da cd pra ele
-# mkch() {
-#       mkdir -p "$1" && cd "$1"
-# }
+# Cria uma pasta e automaticamente muda o diretório para a mesma.
+mkcd() {
+        mkdir -p "$1" && cd "$1"
+}
+
+# Caso haja um arquivo TODO.md, o neovim já abre nele.
+nvim() {
+        if [ $# -eq 0 ] && [ -f "TODO.md" ]; then
+                nvim TODO.md
+        else
+                nvim "$@"
+        fi
+}
