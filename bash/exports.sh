96c3a3cb8de5e84a0a88fe2e4b39849f7b559468
Diogo Lima <dev.diogolima@gmail.com>
2026-02-16 15:32:21 -0300

    Adição de java_home aos exports do bash e mudanças visuais tmux

--- bash/exports.sh
+++ bash/exports.sh
@@ -2,3 +2,6 @@
 
 # Exemplo de modificação
 # export PATH+="append-path"
+
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64/
+
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -46,6 +46,6 @@ require("lazy").setup({
             },
           })
     end,
-    },
+  },
 
 })
